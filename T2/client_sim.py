6237b81a8b3b6808a4218d5cdad40c06cadc37b1
Nupoor Assudani <nupoorassudani@gmail.com>
2026-03-16 18:03:22 +0530

    trials

---
+++ T2/client_sim.py
@@ -0,0 +1,272 @@
+"""
+simulation of user behaviour
+sends schema to server
+then streams data
+
+user will specify all details in schema using json
+the dictionary will have table_name and then dictionaries defining those columns
+a column dictionary will have key value pairs as column_name:[column data type, contraint1, constraint2]
+the table constraints will also be in the same dictionary, as "table@constraints":[constraint1, constraint2]
+the chosen schema is the class university example
+
+the schema is stored in uni_schema.json, imported from there
+
+telemetre is an elec instrument which measures and sends recorded data continuously to some distant station
+
+based on which tables have foreign keys, and which tables they refer to, the order of record creation should be
+timeslot
+classroom
+department
+student
+instructor
+course
+section
+teaches
+takes
+advisor
+prereq
+
+for every record you add, the logic should be
+choose only from valid foreign refs
+"""
+
+from fastapi import FastAPI
+from faker import Faker
+from sse_starlette.sse import EventSourceResponse
+from datetime import datetime, timedelta
+import random
+import asyncio
+import json
+from pathlib import Path
+
+schema_path = Path(__file__).parent / "uni_schema.json" # getting uni_schema.json from parent dir
+with open(schema_path) as f:
+    uni_schema = json.load(f)
+
+app = FastAPI() # app
+faker = Faker() # faker generator
+
+DEPT_POOL = ["Arts", "Maths", "Physics", "Biology", "History", "Geography"] # varchar 20
+COURSE_POOL = []
+CLASSROOM_POOL = []
+TIMESLOT_POOL = []
+SECTION_POOL = []
+STUDENT_POOL = []
+INSTRUCTOR_POOL = []
+
+def trim_name(name, max_len=20):
+    if len(name) <= max_len:
+        return name
+    return name[:max_len].rsplit(" ", 1)[0]
+
+names = [trim_name(faker.unique.name()) for _ in range(1100)]
+STUDENT_NAMES = names[:1000] # maintaining student and instructor pools to prevent overlap
+S_NAME_ID = {} # create ID if not yet else use the one prev created since ID won't change
+INSTRUCTOR_NAMES = names[1000:]
+I_NAME_ID = {}
+
+COURSE_TITLES = [faker.sentence(nb_words=3)[:50] for _ in range(100)]
+
+suffixes = ["Hall", "Building", "Center", "Complex", "Tower"]
+BUILDINGS = [(faker.street_name().split()[0]+" "+random.choice(suffixes)) for _ in range(20)]
+
+def gen_timeslot():
+    ts_id = faker.bothify(text="T###") # bothify gens strs, match pattern, here # replaced with 0-9, eg T320
+    day = random.choice(["Monday","Tuesday","Wednesday","Thursday","Friday"])
+    start_hour = random.randint(8, 16)
+    start_time = f"{start_hour}:00"
+    end_time = f"{start_hour}:50"
+    record = {
+        "table": "time_slot",
+        "time_slot_id": ts_id,
+        "day": day,
+        "start_time": start_time,
+        "end_time": end_time
+    }
+    TIMESLOT_POOL.append((ts_id, day, start_time)) # will need this as many tables refer to this
+    return record
+
+def gen_classroom():
+    building = random.choice(BUILDINGS)
+    room = faker.bothify(text="R###")[:7]
+    record = {
+        "table": "classroom",
+        "building": building,
+        "room_no": room,
+        "capacity": random.randint(20, 200)
+    }
+    CLASSROOM_POOL.append((building, room))
+    return record
+
+def gen_dept():
+    if not CLASSROOM_POOL:
+        gen_classroom() # need there to be buildings before giving a dept a building
+    return {
+        "table": "department",
+        "dept_name": random.choice(DEPT_POOL),
+        "building": random.choice(BUILDINGS), # varchar 30
+        "budget": round(random.uniform(500000, 5000000), 2) # max digs 12, max left to dec pt 2
+    }
+
+def gen_student():
+    name = random.choice(STUDENT_NAMES)
+    if name not in S_NAME_ID.keys():
+        S_NAME_ID[name]=faker.bothify(text="S####")
+    record = {
+        "table": "student",
+        "ID": S_NAME_ID[name], # varchar 5
+        "name": name, # varchar 20
+        "dept_name": random.choice(DEPT_POOL), # varchar 20
+        "tot_cred": random.randint(16, 32) # int
+    }
+    return record
+
+def gen_instructor():
+    name = random.choice(INSTRUCTOR_NAMES) # same logic as student
+    if name not in I_NAME_ID.keys():
+        I_NAME_ID[name]=faker.bothify(text="I####")
+    record = {
+        "table": "instructor",
+        "ID": I_NAME_ID[name], # varchar 5
+        "name": name, # varchar 20
+        "dept_name": random.choice(DEPT_POOL),
+        "salary": round(random.uniform(100000, 1000000), 2) # max 10 digs total, max 2 after dec pt
+    }
+    return record
+
+def gen_course():
+    course_id = faker.bothify(text="CS####")
+    record = {
+        "table": "course",
+        "course_id": course_id,
+        "title": random.choice(COURSE_TITLES),
+        "dept_name": random.choice(DEPT_POOL),
+        "credits": random.randint(1, 5)
+    }
+    COURSE_POOL.append(course_id)
+    return record
+
+def gen_section():
+    if not COURSE_POOL: # fix
+        gen_course()
+    if not CLASSROOM_POOL:
+        gen_classroom()
+    if not TIMESLOT_POOL:
+        gen_timeslot()
+    course = random.choice(COURSE_POOL) # fk
+    building, room = random.choice(CLASSROOM_POOL) # fk
+    ts_id, _, _ = random.choice(TIMESLOT_POOL) # fk
+    sec_id = faker.bothify(text="SEC##") # varchar 8
+    semester = random.choice(["Fall","Spring","Summer","Winter"]) # varchar 6
+    year = random.randint(2021, 2025)
+    record = {
+        "table": "section",
+        "course_id": course,
+        "sec_id": sec_id,
+        "semester": semester,
+        "year": year,
+        "building": building,
+        "room_no": room,
+        "time_slot_id": ts_id
+    }
+    SECTION_POOL.append((course, sec_id, semester, year))
+    return record
+
+def gen_takes():
+    if not STUDENT_POOL:
+        gen_student()
+    if not SECTION_POOL:
+        gen_section()
+    student = random.choice(STUDENT_POOL)
+    course, sec, sem, year = random.choice(SECTION_POOL)
+    return {
+        "table": "takes",
+        "ID": student,
+        "course_id": course,
+        "sec_id": sec,
+        "semester": sem,
+        "year": year,
+        "grade": random.choice(["A","B","C","D","F","A-","B+"])
+    }
+
+def gen_advisor():
+    if not STUDENT_POOL:
+        gen_student()
+    if not INSTRUCTOR_POOL:
+        gen_instructor()
+    return {
+        "table": "advisor",
+        "s_id": random.choice(STUDENT_POOL),
+        "i_id": random.choice(INSTRUCTOR_POOL)
+    }
+
+def gen_prereq():
+    if len(COURSE_POOL) < 20: 
+        gen_course()
+        gen_course()
+    course = random.choice(COURSE_POOL)
+    prereq = random.choice(COURSE_POOL)
+    while prereq == course: # they shouldn't be the same
+        prereq = random.choice(COURSE_POOL)
+    return {
+        "table": "prereq",
+        "course_id": course,
+        "prereq_id": prereq
+    }
+
+TABLE_GENERATORS = {
+    "student": gen_student,
+    "instructor": gen_instructor,
+    "department": gen_dept,
+    "course": gen_course,
+    "section": gen_section,
+    "classroom": gen_classroom,
+    "time_slot": gen_timeslot,
+    "takes": gen_takes,
+    "teaches": gen_advisor,
+    "advisor": gen_advisor,
+    "prereq": gen_prereq
+}
+
+"""
+need to add some bias and randomness to mimic real user input
+for valid input, first need to add some initial records to ensure foreign key matches
+"""
+
+def init_tables():
+    global initiated_tabs
+    if initiated_tabs:
+        return
+    for _ in range(10):
+        gen_timeslot()
+    
+    for _ in range(6):
+        gen_dept()
+    for _ in range(10):
+        gen_student()
+        gen_instructor()
+    for _ in range(10):
+        gen_course()
+    for _ in range(10):
+        gen_classroom()
+    
+    for _ in range(10):
+        gen_section()
+    initiated_tabs = True
+
+@app.get("/") # HTTP endpoint method GET, URL /
+async def single_record():
+    return generate_record()
+
+@app.get("/record/{count}") # HTTP endpoint method GET, URL /record/100 say
+async def stream_records(count: int):
+    async def event_generator():
+        yield { # schema first
+            "event": "schema",
+            "data": json.dumps(uni_schema)
+        }
+        await asyncio.sleep(0.1)
+        for _ in range(count): # then records
+            await asyncio.sleep(0.01)
+            yield {"event": "record", "data": json.dumps(generate_record())}
+    return EventSourceResponse(event_generator())
