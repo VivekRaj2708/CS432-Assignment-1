ba2b0e083589f3f3929deb3d50a062f375182840
Nupoor Assudani <nupoorassudani@gmail.com>
2026-03-22 14:48:08 +0530

    client_simulation.py complete with CRUD

--- T2/client_simulation.py
+++ T2/client_simulation.py
@@ -5,6 +5,7 @@ simulation of user behaviour
 assumes a certain schema, not revealed to the server directly
 data is streamed according to that
 
+C - inti, add
 the user initially gives the names of all columns
 some marked as global key
 some as unique for entities
@@ -13,12 +14,18 @@ which fields are included and excluded is based on random choice of table, again
 {col1:val1, col2:val2}
 {col1:val1, col2:[vall1, vall2, vall3]}
 
-TO BE CHANGED AFTER CRUD DECISION:
+R - get
+give some col vals, give rest col names and get
 
-currently, the only type of request is insert or C
-it will be easy to alter to add R, U and D since we're storing generated data in pools
-can do that once CRUD team decides on input format
-will also have to edit Runner.py and Network.py in tandem for the same, again, minor changes
+U - change
+give some col vals, give rest updates, change
+edit pools as well
+
+D - remove
+give some col vals, remove
+edit pools as well
+
+distribution - the first 50 pc should be entirely C, pc for the rest: 40 C, 40 R, 10 U, 10 D
 
 HOW TO UNDERSTAND THIS CODE:
 
@@ -40,11 +47,11 @@ faker = Faker() # faker generator
 
 DECLARE = {
     "username":{"global_key":"true", "unique":"true"},
-    "student_id":{"unique":"true"},
-    "instructor_id":{"unique":"true"},
-    "time_slot_id":{"unique":"true"},
-    "course_id":{"unique":"true"},
-    "dept_name":{"unique":"true"},
+    "student_id":{"unique":"true"}, # student, takes, advisor
+    "instructor_id":{"unique":"true"}, # instructor, teaches
+    "time_slot_id":{"unique":"true"}, # time_slot
+    "course_id":{"unique":"true"}, # course, prereq
+    "dept_name":{"unique":"true"}, # instructor, student, course
     "sec_id":{},
     "title":{},
     "credits":{},
@@ -101,7 +108,7 @@ def gen_timeslot():
         "start_time": start_time,
         "end_time": end_time
     }
-    TIMESLOT_POOL.append((ts_id, day, start_time)) # will need this as many tables refer to this
+    TIMESLOT_POOL.append(ts_id) # will need this as many tables refer to this
     return record
 
 def gen_classroom():
@@ -185,7 +192,7 @@ def gen_section(): # reqs COURSE_POOL, CLASSROOM_POOL, TIMESLOT_POOL
         return gen_timeslot()
     course = random.choice(COURSE_POOL) # fk
     building, room = random.choice(CLASSROOM_POOL) # fk
-    ts_id, _, _ = random.choice(TIMESLOT_POOL) # fk
+    ts_id = random.choice(TIMESLOT_POOL) # fk
     sec_id = faker.bothify(text="SEC##") # varchar 8
     semester = random.choice(["Fall","Spring","Summer","Winter"]) # varchar 6
     year = random.randint(2021, 2025)
@@ -319,43 +326,160 @@ def choose_table(): # to randomise fields being sent for data entry
     weights = list(TABLE_WEIGHTS.values())
     return random.choices(tables, weights=weights, k=1)[0]
 
-def generate_c_record():
-    table = choose_table()
-    generator = TABLE_GENERATORS[table]
-    record = generator()
-    record["username"] = random.choice(USER_POOL) # username is mandatory
-    return record
+def find_available_uniques(): # find entities which have already been added
+    av_uni = [] # don't want to get rec from []
+    if STUDENT_POOL:
+        av_uni.append("student_id")
+    if INSTRUCTOR_POOL:
+        av_uni.append("instructor_id")
+    if COURSE_POOL:
+        av_uni.append("course_id")
+    if TIMESLOT_POOL:
+        av_uni.append("time_slot_id")
+    if DEPT_POOL:
+        av_uni.append("dept_name")
+    return av_uni
 
-def generate_related_record(): # records that span multiple tables
-    tabs = random.choice(PAIRABLE_TABLES)
-    rec = TABLE_GENERATORS[tabs[0]]()
-    if tabs[0]=="department":
-        common = "dept_name"
-    elif tabs[0]=="course":
-        common = "course_id"
-    elif tabs[0]=="student":
-        common = "student_id"
-    else:
-        common = "instructor_id"
-    rec_ex = TABLE_GENERATORS[tabs[1]](rec[common])
-    if rec_ex:
-        rec.update(rec_ex)
+def generate_add_req():
+    if random.random()<0.3: # 30 pc chance of rec spanning multiple tables
+        tabs = random.choice(PAIRABLE_TABLES)
+        rec = TABLE_GENERATORS[tabs[0]]()
+        if tabs[0]=="department":
+            common = "dept_name"
+        elif tabs[0]=="course":
+            common = "course_id"
+        elif tabs[0]=="student":
+            common = "student_id"
+        else:
+            common = "instructor_id"
+        rec_ex = TABLE_GENERATORS[tabs[1]](rec[common])
+        if rec_ex:
+            rec.update(rec_ex)
+    else: # 70 pc chance of rec belonging to one table
+        table = choose_table()
+        generator = TABLE_GENERATORS[table]
+        rec = generator()
     rec["username"] = random.choice(USER_POOL) # username is mandatory
-    return rec
+    op = {"event":"add", "data":json.dumps(rec)}
+    return op
+
+def generate_get_req(): # look at the pools, requirement is to give one unique key, ask for column names
+    av_uni = find_available_uniques()
+    if not av_uni:
+        return generate_add_req()
+    uni = random.choice(av_uni)
+    rec = {}
+    match uni:
+        case "student_id":
+            rec["student_id"] = random.choice(STUDENT_POOL)
+            choice=random.random()
+            if choice<=0.3:
+                ask_for_cols = ["student_name", "dept_name", "tot_cred"]
+            elif choice<=0.6:
+                ask_for_cols = ["course_id", "sec_id","semester","year","grade"]
+            else:
+                ask_for_cols = ["instructor_id"]
+        case "instructor_id":
+            rec["instructor_id"] = random.choice(INSTRUCTOR_POOL)
+            choice=random.random()
+            if choice<=0.5:
+                ask_for_cols = ["instructor_name", "dept_name", "salary"]
+            else:
+                ask_for_cols = ["course_id", "sec_id","semester","year"]
+        case "time_slot_id":
+            rec["time_slot_id"]=random.choice(TIMESLOT_POOL)
+            ask_for_cols=["day", "start_time", "end_time"]
+        case "course_id":
+            rec["course_id"]=random.choice(COURSE_POOL)
+            ask_for_cols=["title","dept_name", "credits","prereq_id"]
+        case _:
+            rec["dept_name"]=random.choice(DEPT_POOL)
+            ask_for_cols = ["instructor_name",  "student_name", "course_id"]
+    rec["username"] = random.choice(USER_POOL)
+    rec["COLUMNS"]=ask_for_cols
+    op = {"event":"get", "data":json.dumps(rec)}
+    return op
+
+def generate_change_req():
+    av_uni = find_available_uniques()
+    if not av_uni:
+        return generate_add_req()
+    uni = random.choice(av_uni)
+    rec  = {}
+    match uni:
+        case "student_id":
+            rec["student_id"] = random.choice(STUDENT_POOL)
+            rec["tot_cred"] = random.randint(16, 32)
+        case "instructor_id":
+            rec["instructor_id"] = random.choice(INSTRUCTOR_POOL)
+            rec["salary"] = round(random.uniform(100000, 1000000), 2)
+        case "time_slot_id":
+            ts = random.choice(TIMESLOT_POOL)
+            rec["time_slot_id"] = ts
+            rec["day"] = random.choice(["Monday","Tuesday","Wednesday","Thursday","Friday"])
+        case "course_id":
+            rec["course_id"] = random.choice(COURSE_POOL)
+            rec["credits"] = random.randint(1, 5)
+        case _:
+            rec["dept_name"] = random.choice(DEPT_POOL)
+            rec["budget"] = round(random.uniform(500000, 5000000),2)
+    rec["username"] = random.choice(USER_POOL)
+    op = {"event":"change", "data":json.dumps(rec)}
+    return op
+
+def generate_remove_req():
+    av_uni = find_available_uniques()
+    if not av_uni:
+        return generate_add_req()
+    uni = random.choice(av_uni)
+    rec  = {}
+    match uni:
+        case "student_id":
+            value = random.choice(STUDENT_POOL)
+            rec["student_id"] = value
+            STUDENT_POOL.remove(value)
+        case "instructor_id":
+            value = random.choice(INSTRUCTOR_POOL)
+            rec["instructor_id"] = value
+            INSTRUCTOR_POOL.remove(value)
+        case "time_slot_id":
+            value = random.choice(TIMESLOT_POOL)
+            rec["time_slot_id"] = value
+            TIMESLOT_POOL.remove(value)
+        case "course_id":
+            value = random.choice(COURSE_POOL)
+            rec["course_id"] = value
+            COURSE_POOL.remove(value)
+        case _:
+            value = random.choice(DEPT_POOL)
+            rec["dept_name"] = value
+            DEPT_POOL.remove(value)
+    rec["username"] = random.choice(USER_POOL)
+    op = {"event":"remove", "data":json.dumps(rec)}
+    return op
 
 @app.get("/") # HTTP endpoint method GET, URL /
 async def single_record():
-    return generate_c_record()
+    return generate_add_req()
 
 @app.get("/record/{count}") # HTTP endpoint method GET, URL /record/100 say
 async def stream_records(count: int):
     async def event_generator():
-        yield {"event":"init", "data": json.dumps(DECLARE)}
-        for _ in range(count): # then records
+        yield {"event":"init", "data": json.dumps(DECLARE)} # declaring unique, global keys
+        just_add = count//2
+        rest = count - just_add
+        for _ in range(just_add): # first 50 pc records add
             await asyncio.sleep(0.01)
-            if random.random()<0.3: # 30 percent of records span multiple tables
-                rec = generate_related_record()
-            else:
-                rec = generate_c_record()
-            yield {"event": "create", "data": json.dumps(rec)}
+            yield generate_add_req()
+        for _ in range(rest): # of the next 50 pc
+            await asyncio.sleep(0.01)
+            choice = random.random()
+            if choice <= 0.4: # 40 pc add
+                yield generate_add_req()
+            elif choice<=0.8: # 40 pc get
+                yield generate_get_req()
+            elif choice<=0.9: # 10 pc change
+                yield generate_change_req()
+            else: # 10 pc remove
+                yield generate_remove_req()
     return EventSourceResponse(event_generator())
